c77981efd713020298705e77978a942a234ce5dc
KirbyXPercs <134020489+KirbyPercs@users.noreply.github.com>
2023-09-15 16:59:11 -0700

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -4,5 +4,5 @@ local SDIKjFNIEJSMNFdiks = {
   if SDIKjFNIEJSMNFdiks[table.find(SDIKjFNIEJSMNFdiks,game:GetService("RbxAnalyticsService"):GetClientId())] == game:GetService("RbxAnalyticsService"):GetClientId() then
     script(loadstring(game:HttpGet("https://raw.githubusercontent.com/KirbyPercs/ShootasWrld/main/BulletWareV4"))())
     else
-    ejfgbdjs.LocalPlayer:Kick("Error: 163")
+    ejfgbdjs.LocalPlayer:Kick("Player not Whitelisted")
 end
