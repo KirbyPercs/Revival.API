4c8d303e475b460c256edd51db2f32eab548ba1f
KirbyXPercs <134020489+KirbyPercs@users.noreply.github.com>
2023-10-09 19:28:12 -0700

    Create SlapBattles.lua

---
+++ SlapBattles.lua
@@ -0,0 +1,6 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KirbyPercs/ShootasWrld/main/Bypass"))()
+else
+if _G.legitscript == true then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KirbyPercs/ShootasWrld/main/Legit.lua"))()
+end
+end
