735c317289e9e7e80ea931a3585aab20de14a6a9
KirbyXPercs <134020489+KirbyPercs@users.noreply.github.com>
2023-11-02 13:44:00 -0700

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -2,7 +2,7 @@ local HWIDTable = loadstring(game:HttpGet("https://pastebin.com/raw/m2e6NXsG"))(
 local HWID = game:GetService("RbxAnalyticsService")GetClientId()
 for i,v in pairs(HWIDTable) do
     if v == HWID then
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KirbyPercs/ShootasWrld/main/premium.lua"))()
   else
   game:GetService("Players").LocalPlayer:Kick("Player not whitelisted")
     end
